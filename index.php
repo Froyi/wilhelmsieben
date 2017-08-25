c7a5650bc94d633a6fd126b91703cf7be732def2
Maik <froyi1986>
2017-08-25 16:42:07 +0200

    -Slider added - Sidebar Text update - optical changes for text

--- index.php
+++ index.php
@@ -6,20 +6,6 @@ define('ROOT_PATH', getcwd());
 
 require ROOT_PATH . '/vendor/autoload.php';
 
-/*use Project\Module\Database\Database;
-use Project\View\ViewRenderer;
-
-$configuration = new Configuration();
-$renderer = new ViewRenderer($configuration);
-$database = Database::getInstance();
-
-$template = 'index.twig';
-
-$variables = ['page' => 'home'];
-
-$renderer->renderTemplate($template, $variables);*/
-
-
 $route = 'index';
 
 if (isset($_GET['route'])) {
