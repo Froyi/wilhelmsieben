83e3ad03e9709e154f26aa80953b68900304c9a2
Maik <froyi1986>
2017-09-06 13:10:27 +0200

    - added more slider elements

--- config.php
+++ config.php
@@ -9,18 +9,18 @@ return [
         'dir' =>  '/cafe',
         'cacheDir' => '/cache'
     ],
-    'database' => [
+    /*'database' => [
         'host' => 'localhost',
         'user' => 'web28634274',
         'password' => 'q3qKQMpf',
         'database' => 'usr_web28634274_1'
-    ],
-    /*'database' => [
+    ],*/
+    'database' => [
         'host' => 'localhost',
         'user' => 'root',
         'password' => '',
         'database' => 'cafe'
-    ],*/
+    ],
     'controller' => [
         'namespace' => 'Controller'
     ],
@@ -33,5 +33,10 @@ return [
             'controller' => 'BasicController',
             'action' => 'notFoundAction'
         ]
+    ],
+    'slider' => [
+        '/img/slider/teaser.jpg',
+        '/img/slider/teaser2.jpg',
+        '/img/slider/teaser3.jpg',
     ]
 ];
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -15,10 +15,14 @@ class IndexController extends DefaultController
         $newsService = new NewsService($database);
         $allNews = $newsService->getAllNewsOrderByDate();
 
+        $slider = $this->configuration->getEntryByName('slider');
+        shuffle($slider);
+
         $pageTemplate = 'index.twig';
         $config = [
             'page' => 'home',
             'news' => $allNews,
+            'slider' => $slider,
             'dailySoup' => [
                 'day' => 'Mo|31.07.2017',
                 'soup' => ['Erbsensuppe', 'Linsensuppe']
